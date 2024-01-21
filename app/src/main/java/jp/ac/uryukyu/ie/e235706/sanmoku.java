71d2b2c8e47a53a90d67a0f6269c32e9dd2c068a
Takuma Higa <hikatakushin@hikatakushinnoMacBook-Air.local>
2024-01-21 21:04:56 +0900

    second commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235706/sanmoku.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235706/sanmoku.java
@@ -1,5 +1,18 @@
 package jp.ac.uryukyu.ie.e235706;
 
+import java.util.Scanner;
+
 public class sanmoku {
-    
+
+    public static void 名前を決める(){
+        System.out.println("マルを選んだ人の名前を入力してください");
+        Scanner nameA = new Scanner(System.in);
+        String name1 =nameA.next();
+        System.out.println("バツを選んだ人の名前を入力してください");
+        Scanner nameB = new Scanner(System.in);
+        String name2 = nameB.next();
+        System.out.println("〇 のプレイヤー："+name1);
+        System.out.println("× のプレイヤー："+name2);
+    }
 }
+    
